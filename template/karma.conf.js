c3bbeb0e8ce2907ee3fa7b8023be1c450f0f301a
Steve Mathews <stevenmathews.nz@gmail.com>
2016-01-14 17:32:00 +1300

    Add webpack with sourcemaps for preprocessing

--- template/karma.conf.js
+++ template/karma.conf.js
@@ -1,16 +1,30 @@
 'use strict'
 
+const webpackConfigLoaders = require('./webpack.config.js').module.loaders
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     files: [
-      'test/**/*.js'
+      'test/test-index.js'
     ],
     exclude: [
     ],
     frameworks: ['mocha', 'sinon-chai'],
     browsers: ['Chrome'],
     reporters: ['mocha'],
+    preprocessors: {
+      'test/test-index.js': ['webpack', 'sourcemap']
+    },
+    webpack: {
+      module: {
+        loaders: webpackConfigLoaders
+      },
+      devtool: 'inline-source-map'
+    },
+    webpackMiddleware: {
+      noInfo: true
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
