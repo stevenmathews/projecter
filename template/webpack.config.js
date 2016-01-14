ae8cf76a823055c7f54f4d7e3112a2788f5f5829
Steve Mathews <stevenmathews.nz@gmail.com>
2016-01-14 17:06:11 +1300

    Add head and index to generated index.html

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,8 +1,15 @@
 var getConfig = require('hjs-webpack')
+var layout = require('./src/pre-render/layout')
+var head = require('./src/pre-render/head')
  
 module.exports = getConfig({
- in: 'src/app.js',
- out: 'public',
- clearBeforeBuild: true,
- isDev: process.env.NODE_ENV !== 'production'
+  in: 'src/app.js',
+  out: 'public',
+  clearBeforeBuild: true,
+  isDev: process.env.NODE_ENV !== 'production',
+  html: function (data) {
+    return {
+      'index.html': data.defaultTemplate({html: layout, head: head})
+    }
+  }
 })
